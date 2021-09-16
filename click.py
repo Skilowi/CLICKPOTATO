8bf294de020695c79a9ca8713104a51ff1426b44
Skilowi <76708049+Skilowi@users.noreply.github.com>
2021-09-16 22:44:21 +0200

    Update click.py

--- click.py
+++ click.py
@@ -1,15 +1,4 @@
 from tkinter import *
-
-win = Tk()
-win.title("CLICK POTATO!")
-win.geometry("400x200")
-win.configure(bg="black")
-
-Punkty = 0
-
-def CLICK():
-	global Punkty
-	global WYNIKfrom tkinter import *
 import os
 
 win = Tk()
@@ -45,25 +34,5 @@ ZIEMNIORY = Label(text = "CLICKED POTATOES:", bg = "black", fg = "white" )
 ZIEMNIORY.pack(side = "bottom")
 
 WYJSCIE = Button(win, text = "EXIT", command = EXIT, height = 2, width = 7, bg = "black", fg = "white" ).place(x=0, y=0)
-
-mainloop()
-
-	global ZIEMNIORY
-	WYNIK.destroy()
-	ZIEMNIORY.destroy()
-	Punkty += 1
-	WYNIK = Label(text = Punkty, bg = "black", fg = "white" )
-	WYNIK.pack(side = "bottom")
-	ZIEMNIORY = Label(text = "CLICKED POTATOES:", bg = "black", fg = "white" )
-	ZIEMNIORY.pack(side = "bottom")
-
-ZIEMNIOK = Button(win, text = "POTATO!", command = CLICK, height = 2, width = 7, bg = "black", fg = "white" )
-ZIEMNIOK.pack(side = "top")
-
-WYNIK = Label(text = Punkty , bg = "black", fg = "white" )
-WYNIK.pack(side = "bottom")
-
-ZIEMNIORY = Label(text = "CLICKED POTATOES:", bg = "black", fg = "white" )
-ZIEMNIORY.pack(side = "bottom")
 
 mainloop()
